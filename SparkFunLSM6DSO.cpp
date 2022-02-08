5c3e1ecfeeb1cc3b9c35f1a2696b009718d38818
JonMcDonald1 <54954265+JonMcDonald1@users.noreply.github.com>
2022-02-07 21:59:44 -0500

    Update SparkFunLSM6DSO.cpp

--- SparkFunLSM6DSO.cpp
+++ SparkFunLSM6DSO.cpp
@@ -214,26 +214,25 @@ LSM6DSO::LSM6DSO()
 {
 	//Construct with these default imuSettings
 
-	imuSettings.gyroEnabled = true;  //Can be 0 or 1
-	imuSettings.gyroRange = 500;   //Max deg/s.  Can be: 125, 250, 500, 1000, 2000
-	imuSettings.gyroSampleRate = 416;   //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666
-	imuSettings.gyroBandWidth = 400;  //Hz.  Can be: 50, 100, 200, 400;
-	imuSettings.gyroFifoEnabled = 1;  //Set to include gyro in FIFO
+	imuSettings.gyroEnabled = true;       //Can be 0 or 1
+	imuSettings.gyroRange = 500;          //Max deg/s.  Can be: 125, 250, 500, 1000, 2000
+	imuSettings.gyroSampleRate = 416;     //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666
+	imuSettings.gyroBandWidth = 400;      //Hz.  Can be: 50, 100, 200, 400;
+	imuSettings.gyroFifoEnabled = 1;      //Set to include gyro in FIFO
 	imuSettings.gyroAccelDecimation = 1;  //Set to include gyro in FIFO
 
 	imuSettings.accelEnabled = true;
-	imuSettings.accelRange = 8;      //Max G force readable.  Can be: 2, 4, 8, 16
+	imuSettings.accelRange = 8;         //Max G force readable.  Can be: 2, 4, 8, 16
 	imuSettings.accelSampleRate = 416;  //Hz.  Can be: 1.6 (16), 12.5 (125), 26, 52, 104, 208, 416, 833, 1660, 3330, 6660
-	imuSettings.accelFifoEnabled = 1;  //Set to include accelerometer in the FIFO
+	imuSettings.accelFifoEnabled = 1;   //Set to include accelerometer in the FIFO
 
   imuSettings.fifoEnabled = true;
-	imuSettings.fifoThreshold = 3000;  //Can be 0 to 4096 (16 bit bytes)
+	imuSettings.fifoThreshold = 3000;   //Can be 0 to 4096 (16 bit bytes)
 	imuSettings.fifoSampleRate = 416; 
-	imuSettings.fifoModeWord = 0;  //Default off
+	imuSettings.fifoModeWord = 0;       //Default off
 
 	allOnesCounter = 0;
 	nonSuccessCounter = 0;
-
 
 }
 
@@ -252,7 +251,6 @@ bool LSM6DSO::begin(uint8_t address, TwoWire &i2cPort){
 }
 
 
-
 bool LSM6DSO::initialize(uint8_t settings){
 
   setIncrement();
@@ -263,7 +261,6 @@ bool LSM6DSO::initialize(uint8_t settings){
     setGyroRange(500);
     setGyroDataRate(416);
     setBlockDataUpdate(true);
-    // enablePedometer(true);  FUTURE USE ?
   }
  
 
@@ -1104,15 +1101,15 @@ float LSM6DSO::calcGyro( int16_t input ) {
 //  Temperature section
 //
 //****************************************************************************//
-
+// Removed
 //****************************************************************************//
 //
 //  FIFO section
 //
 //****************************************************************************//
+//Removed 
 
-
-// DO NOT TOUCH THE FOLLOWING FUNCTIONS BELOW
+// DO NOT TOUCH THE FOLLOWING FUNCTIONS BELOW , in initialize()
 
 // Used In initialize function
 // Address: 0x12 , bit[4]: default value is: 0x01
